dcc9f9f70424da51ffd6f7ab306ca49144fd013c
Curlon Phillips <39802748+fanz1st@users.noreply.github.com>
2020-07-31 08:22:07 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "GTMSessionFetcher",
     platforms: [
         .macOS(.v10_10),
-        .iOS(.v8),
+        .iOS(.v12),
         .tvOS(.v9),
         .watchOS(.v2)
     ],
